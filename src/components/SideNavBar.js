9ee101b8701bfd8f7593518f5fcc1ac2f02aac78
kedi1991 <okurut.henry@yahoo.com>
2023-10-25 02:13:08 +0000

    Add the side nav bar with Taks adk project links

---
+++ src/components/SideNavBar.js
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import { Accordion } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
+function SideNavBar() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Launch
+      </Button>
+
+      <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>TaskPRO v1.0</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <Accordion>
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>TASKS</Accordion.Header>
+              <Accordion.Body>
+                <a href='#'>View tasks</a><br/>
+                <a href='#'>Add task</a><br/>
+                <a href='#'>Delete task</a><br/>
+                <a href='#'>Edit task</a><br/>
+
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>PROJECTS</Accordion.Header>
+              <Accordion.Body>
+                <a href='#'>Create project</a><br/>
+                <a href='#'>Edit project</a><br/>
+                <a href='#'>View projects</a><br/>
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
+  );
+}
+
+export default SideNavBar;
