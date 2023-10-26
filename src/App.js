6974abe41f5eb1cebd2e3cc425df25b9eaa4958f
kedi1991 <okurut.henry@yahoo.com>
2023-10-26 00:34:52 +0000

    Add sign in form boilerplate code

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import SideNavBar from './components/SideNavBar';
 import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 import './api/axiosDefaults';
 import RegisterForm from './pages/auth/RegisterForm';
+import SignInForm from './pages/auth/SignInForm';
 
 
 
@@ -15,7 +16,7 @@ function App() {
       <Container>
         <Switch>
           <Route exact path="/" render={() => <h1>Home page</h1>} />
-          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+          <Route exact path="/signin" render={() => <SignInForm></SignInForm>} />
           <Route exact path="/register" render={() => <RegisterForm></RegisterForm> } />
           <Route render={() => <p>The gods are wise, but they can't seem to understand your request!</p>} />
         </Switch>
