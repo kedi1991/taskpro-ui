c2a2082ecb3ce80f08afda3caed9632cb56cb541
kedi1991 <okurut.henry@yahoo.com>
2024-01-18 04:02:08 +0000

    Add axios req and resp objects

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,7 @@ import axios from "axios";
 axios.defaults.baseURL = "https://taskproapi-af20c66822dd.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+//For managing the access token
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
