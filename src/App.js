7cee3cc4c3540eec8d6641a015785f7ca0658c94
kedi1991 <okurut.henry@yahoo.com>
2024-01-18 04:09:38 +0000

    Insert Interceptor code for handle access tokens

--- src/App.js
+++ src/App.js
@@ -37,6 +37,50 @@ function App() {
     handleMount();
   }, []);
 
+
+  //Interseptors to handle the access token
+  useMemo(() => {
+    axiosReq.interceptors.request.use(
+      async (config) => {
+        try {
+          await axios.post("/dj-rest-auth/token/refresh/");
+        } catch (err) {
+          setCurrentUser((prevCurrentUser) => {
+            if (prevCurrentUser) {
+              history.push("/signin");
+            }
+            return null;
+          });
+          return config;
+        }
+        return config;
+      },
+      (err) => {
+        return Promise.reject(err);
+      }
+    );
+
+    axiosRes.interceptors.response.use(
+      (response) => response,
+      async (err) => {
+        if (err.response?.status === 401) {
+          try {
+            await axios.post("/dj-rest-auth/token/refresh/");
+          } catch (err) {
+            setCurrentUser((prevCurrentUser) => {
+              if (prevCurrentUser) {
+                history.push("/signin");
+              }
+              return null;
+            });
+          }
+          return axios(err.config);
+        }
+        return Promise.reject(err);
+      }
+    );
+  }, [history]);
+
   return (
     <ActiveUserContext.Provider value={activeUser}>
       <SetActiveUserContext.Provider value={setActiveUser}>
