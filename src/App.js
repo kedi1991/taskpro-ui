b3beaf2411e22af290c12922509ca752aafa3e5f
kedi1991 <okurut.henry@yahoo.com>
2024-02-25 16:18:38 +0000

    Remove unused imports from App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import { Container } from 'react-bootstrap';
 import './App.css';
-import { Route, Switch, useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import './api/axiosDefaults';
 import RegisterForm from './pages/auth/RegisterForm';
 import SignInForm from './pages/auth/SignInForm';
-import { createContext, useContext, useEffect, useMemo, useState } from 'react';
-import axios from 'axios';
+import { createContext } from 'react';
 import TaskForm from './pages/tasks/TaskForm';
 import TaskView from './pages/tasks/TaskView';
-import TasksView from './pages/tasks/TasksView';
 import ProjectForm from './pages/projects/ProjectForm';
 import ProjectView from './pages/projects/ProjectView';
 import TopNavBar from './components/TopNavBar';
-import { axiosReq, axiosRes } from './api/axiosDefaults';
 import styles from './App.module.css'
 import Tasks from './pages/tasks/Tasks';
 import Projects from './pages/projects/Projects';
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom';
 
 export const ActiveUserContext = createContext();
 export const SetActiveUserContext = createContext();
