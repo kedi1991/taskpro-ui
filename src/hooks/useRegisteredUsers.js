c6ffd53762f1c1727fd6d320f2832c0e331f15eb
kedi1991 <okurut.henry@yahoo.com>
2024-03-02 19:44:11 +0000

    Create acustom hook to create a new task

---
+++ src/hooks/useRegisteredUsers.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useRegisteredUsers = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true); // Add loading state
+
+  useEffect(() => {
+    axios.get('/profiles/')
+      .then(response => {
+        setData(response.data);
+        setLoading(false); // Set loading to false after data is fetched
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        setLoading(false); // Set loading to false in case of error
+      });
+  }, []); // ensures effect runs only once after initial render
+
+  return { data, loading }; // Return data and loading state
+}
+
+export default useRegisteredUsers;
