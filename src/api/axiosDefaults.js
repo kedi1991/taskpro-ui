1f1f84badff9fac42ca03fd6803d521620e2e5c6
kedi1991 <okurut.henry@yahoo.com>
2024-03-03 22:01:45 +0000

    update base url of api

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-//axios.defaults.baseURL = "https://taskproapi-af20c66822dd.herokuapp.com/";
+axios.defaults.baseURL = "/api";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
