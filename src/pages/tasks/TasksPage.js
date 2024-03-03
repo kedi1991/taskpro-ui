9130ac32183c6051073546fd13d3d70258f6b1e6
kedi1991 <okurut.henry@yahoo.com>
2024-03-03 16:16:21 +0000

    Added placeholder to read all tasks

--- src/pages/tasks/TasksPage.js
+++ src/pages/tasks/TasksPage.js
@@ -1,24 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Task from './Task';
 
-import Form from "react-bootstrap/Form";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/Container";
+function TasksPage() {
+  const [tasks, setTasks] = useState([]);
+  const [filter, setFilter] = useState('');
+  const [loading, setLoading] = useState(true);
 
-import appStyles from "../../App.module.css";
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const response = await axios.get(`/tasks?filter=${filter}`);
+        setTasks(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching tasks:', error);
+        setLoading(false);
+      }
+    };
 
-function  TasksPage({message, filter =""}) {
-  
+    fetchTasks();
+  }, [filter]);
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
   return (
-    <Row className="h-100">
-      <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Popular profiles mobile</p>
-        <p>List of posts here</p>
-      </Col>
-      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        <p>Popular profiles for desktop</p>
-      </Col>
-    </Row>
+    <div>
+      <h1>Tasks list</h1>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={filter} 
+        onChange={handleFilterChange}
+      />
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {tasks.map((item) => (
+            <>
+            <Task></Task>
+            </>
+            // <li key={item.id}>{item.task_name}</li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 }
 
